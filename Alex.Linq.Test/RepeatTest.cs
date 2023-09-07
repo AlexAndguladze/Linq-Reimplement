b1f8dde3b06e83363aea1f2650093b9402952589
FASTPAY\a.andghuladze <andghuladze.alex@gmail.com>
2023-09-07 17:58:21 +0400

    Add Repeat and Repeat Tests

---
+++ Alex.Linq.Test/RepeatTest.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alex.Linq.Test
+{
+    [TestFixture]
+    public class RepeatTest
+    {
+        [Test]
+        public void RepeatThreeStrings()
+        {
+            IEnumerable<string> strings1 = Alex.Linq.IEnumerable.Repeat("Alex", 3);
+            IEnumerable<string> strings2 = new List<string>() { "Alex", "Alex", "Alex" };
+            Assert.AreEqual(strings1, strings2);
+        }
+        [Test]
+        public void RepeatReturnsEmptySequence()
+        {
+            IEnumerable<string> strings = Alex.Linq.IEnumerable.Repeat("Alex", 0);
+            Assert.IsEmpty(strings);
+        }
+        [Test]
+        public void RepeatCountCantBeNegative()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => Alex.Linq.IEnumerable.Repeat("Alex", -3));
+        }
+    }
+}
---
+++ Alex.Linq/Repeat.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alex.Linq
+{
+    public static partial class IEnumerable
+    {
+        public static IEnumerable<TResult> Repeat<TResult>(TResult element, int count)
+        {
+            if(count < 0)
+            {
+                throw new ArgumentOutOfRangeException("count");
+            }
+            return RepeatImpl(element, count);
+        }
+        private static IEnumerable<TResult> RepeatImpl<TResult>(TResult element, int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                yield return element;
+            }
+        }
+    }
+}
